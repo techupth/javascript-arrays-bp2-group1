eb17d011c7945f402f52d68143b062cf64854315
Pawat <as.pawat@gmail.com>
2023-12-25 00:26:49 +0800

    pup

--- ex-6.js
+++ ex-6.js
@@ -4,7 +4,9 @@ const student = {
   subjects: ["Math", "Science", "History"],
 };
 
-console.log("Student Name: " + stdent.name);
-console.log("Student Age: " + student.age);
-console.log("Student Grade: " + student.gade);
-console.log("Student Subjects: " + student.subjects);
+student.age = 18
+console.log(student)
+console.log(student.name);
+console.log(student.age);
+console.log(student.grade);
+console.log(student.subjects);
